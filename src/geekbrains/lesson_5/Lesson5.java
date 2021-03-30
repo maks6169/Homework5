ba3a58d82ca991f334dc8aa6136a2c31226da828
maks6169 <maks6169@rambler.ru>
2021-03-30 22:05:38 +0300

    Homework5

--- src/geekbrains/lesson_5/Lesson5.java
+++ src/geekbrains/lesson_5/Lesson5.java
@@ -3,7 +3,7 @@ package geekbrains.lesson_5;
 public class Lesson5 {
 
     public static void main(String[] args) {
-        Employee employee = new Employee("Alexeev A.I.", "Tech.director", "alex4535@mail.com", 89531543498L, 999999, 28);
+        Employee5 employee = new Employee5("Alexeev A.I.", "Tech.director", "alex4535@mail.com", 89531543498L, 999999, 28);
         employee.initials = "Alexeev A.I.";
         employee.position = "Tech.director";
         employee.email = "alex4535@mail.com";
@@ -12,16 +12,16 @@ public class Lesson5 {
         employee.age = 28;
         System.out.printf(" Employee %s\n Position %s\n Age %d\n", employee.initials, employee.position, employee.age);
         System.out.println();
-        Employee employee1 = new Employee();
+        Employee5 employee1 = new Employee5();
         employee.getInfo();
 
         // Задание 4
-        Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Batman B. W.", 48);
-        employees[1] = new Employee("Superman C. C.", 45);
-        employees[2] = new Employee("WonderWoman G. G.", 25);
-        employees[3] = new Employee("Flash B. A.", 20);
-        employees[4] = new Employee("AquaMan A. B.", 41);
+        Employee5[] employees = new Employee5[5];
+        employees[0] = new Employee5("Batman B. W.", 48);
+        employees[1] = new Employee5("Superman C. C.", 45);
+        employees[2] = new Employee5("WonderWoman G. G.", 25);
+        employees[3] = new Employee5("Flash B. A.", 20);
+        employees[4] = new Employee5("AquaMan A. B.", 41);
 
         // Задание 5
         for (int i = 0; i < employees.length; i++) {
