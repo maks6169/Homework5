c653435195935ca4b986bb965422414bf4a0d161
maks6169 <maks6169@rambler.ru>
2021-03-30 21:55:25 +0300

    Initial commit

---
+++ src/geekbrains/lesson_5/Employee.java
@@ -0,0 +1,45 @@
+package geekbrains.lesson_5;
+
+public class Employee {
+
+    // Задание 1
+    String initials;
+    String position;
+    String email;
+    long phoneNumber;
+    long salary; //long потому что я оптимист)
+    int age;
+
+    public Employee(String initials, String position, String email, long phoneNumber, long salary, int age) {
+        this.initials = initials;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public Employee(String initials, String position, long salary, int age) {
+        this.initials = initials;
+        this.position = position;
+        this.salary = salary;
+        this.age = age;
+
+    }
+    public Employee(String initials, int age){
+        this.initials = initials;
+        this.age = age;
+    }
+
+    // Задание 2
+    public Employee() {
+        this("Pupkin A. V.", "manager", 90000, 35);
+        System.out.printf(" Employee %s\n Position %s\n Age %d\n", initials, position, age);
+    }
+
+    // Задание 3
+    public void getInfo() {
+        System.out.println();
+        System.out.printf(" Employee %s\n Position %s\n Age %d\n email %s\n phoneNumber %d\n", initials, position, age, email, phoneNumber);
+    }
+}
